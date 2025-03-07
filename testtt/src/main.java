61dd7b52e3b88542013f5836363a2be3791b99cd
MYAR025 <M.Saad2387@nu.edu.eg>
2025-03-07 22:33:36 +0200

    Delete testtt directory

--- testtt/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        circle c = new circle(5);
-        System.out.println(c.getArea());
-        System.out.println(c.getPerimeter());
-        System.out.println("version2");
-    }
-}
